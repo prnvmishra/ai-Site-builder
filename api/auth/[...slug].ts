93e1a73ebfac9cdce398f8e6589a7c541bec091f
ankitaa2603 <ankitaa2603@gmail.com>
2026-01-02 20:45:56 +0530

    Add API routes for Vercel deployment

---
+++ api/auth/[...slug].ts
@@ -0,0 +1,7 @@
+import { toNodeHandler } from 'better-auth/node'
+import { auth } from '../../server/lib/auth.js'
+
+export default toNodeHandler(auth)
+export const config = {
+  runtime: 'nodejs',
+}
