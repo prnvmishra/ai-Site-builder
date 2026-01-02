a22ac4c9a9fdf00800f778aeb769562c2af96d1d
ankitaa2603 <ankitaa2603@gmail.com>
2026-01-02 20:47:39 +0530

    Fix API imports and add Vercel dependencies

--- api/auth/[...slug].ts
+++ api/auth/[...slug].ts
@@ -1,5 +1,5 @@
 import { toNodeHandler } from 'better-auth/node'
-import { auth } from '../../server/lib/auth.js'
+import { auth } from '../../../server/lib/auth.js'
 
 export default toNodeHandler(auth)
 export const config = {
