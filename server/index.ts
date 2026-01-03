d4b18d4da4ded25fb0ebda707c91f67c961500f1
ankitaa2603 <ankitaa2603@gmail.com>
2026-01-03 14:08:32 +0530

    Add environment variables test endpoint

--- server/index.ts
+++ server/index.ts
@@ -26,6 +26,18 @@ app.use(express.json({limit: '50mb'}))
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+app.get('/api/test-env', (req: Request, res: Response) => {
+    res.json({
+        message: 'Environment variables test',
+        hasDatabaseUrl: !!process.env.DATABASE_URL,
+        hasAuthSecret: !!process.env.BETTER_AUTH_SECRET,
+        hasAuthUrl: !!process.env.BETTER_AUTH_URL,
+        hasTrustedOrigins: !!process.env.TRUSTED_ORIGINS,
+        nodeEnv: process.env.NODE_ENV
+    });
+});
+
 app.use('/api/user', userRouter);
 app.use('/api/project', projectRouter);
 
